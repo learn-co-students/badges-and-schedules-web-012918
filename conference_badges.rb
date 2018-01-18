25ceb3b312ab49fa95f6080df1b6f2e379895615
jaf7 <janthonyfields@gmail.com>
2018-01-17 19:34:20 -0500

    Used iteration output lists and called methods with methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def  badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.collect { |name| badge_maker(name) }
+end
+
+def assign_rooms(speakers_array)
+  room_assignments = []
+  speakers_array.each_with_index do |speaker, index|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |room_assignment| puts room_assignment }
+end
