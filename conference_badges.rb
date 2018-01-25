f890bf21b8738bb431b1cf0926e2c2fb178433d2
alin684 <al684@cornell.edu>
2018-01-24 23:49:09 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  new_array
+end
+def assign_rooms(list)
+  final = []
+  list.each_with_index do |name, index|
+    final.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  final
+end
+def printer(list)
+  batch_badge_creator(list).each do |x|
+    puts x
+  end
+  assign_rooms(list).each do |y|
+    puts y
+  end
+end
